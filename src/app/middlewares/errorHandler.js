ac9b03e0c931935ab57666608ad6c377fc4b9092
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2021-12-05 10:57:50 -0300

    Fixing errorHandler

--- src/app/middlewares/errorHandler.js
+++ src/app/middlewares/errorHandler.js
@@ -1,4 +1,4 @@
-module.exports = (error, request, response) => {
+module.exports = (error, request, response, next) => {
   console.log('#### Error Handler');
   console.log(error);
   response.status(500).send();
